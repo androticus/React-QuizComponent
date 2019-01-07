2848f8657e404dbf34c727ad0a1bc9a87e20dc91
BOULDER\baisa <baisa@webroot.com>
2019-01-07 15:19:01 -0700

    module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,14 +4,16 @@ import QuizQuestionButton from './QuizQuestionButton';
 class QuizQuestion extends Component {
     constructor(props) {
         super(props);
+        this.state = { incorrectAnswer: false};
     }
 
     handleClick(buttonText) {
-       if (buttonText === this.props.quiz_question.answer) {
+        if (buttonText === this.props.quiz_question.answer) {
+           this.setState((state) => {return {incorrectAnswer: false}});
            this.props.showNextQuestionHandler();
-       } else {
-
-       }
+        } else {
+            this.setState((state) => {return {incorrectAnswer: true}});
+        }
 
     }
 
@@ -29,6 +31,7 @@ class QuizQuestion extends Component {
                 
             </ul>
             </section>
+            {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
         </main>
         );
     }
